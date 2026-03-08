1a39c90ef6e6ae9b0d1984102184dfe5267e889c
Pedro Zea <pedrozea@live.com>
2026-03-08 09:28:11 +0100

    fix: update resource group name output to use module reference

--- lab04-secure-hub-spoke-fintech/infrastructure/terraform/root/outputs.tf
+++ lab04-secure-hub-spoke-fintech/infrastructure/terraform/root/outputs.tf
@@ -5,7 +5,7 @@
 # ---- Resource Group ----
 output "resource_group_name" {
   description = "Name of the created resource group"
-  value       = azurerm_resource_group.rg.name
+  value       = module.resource_group.name
 }
 
 # ---- Hub VNet ----
