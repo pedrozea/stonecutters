5807504c9a2135efde23b896dbd9fc3eaf726bab
Pedro Zea <pedrozea@live.com>
2026-03-11 20:07:21 +0100

    feat: Add outputs for DEV and PROD spoke VNet IDs and subnets.

--- lab04-secure-hub-spoke-fintech/infrastructure/terraform/root/outputs.tf
+++ lab04-secure-hub-spoke-fintech/infrastructure/terraform/root/outputs.tf
@@ -16,3 +16,21 @@ output "hub_vnet_id" {
 output "hub_subnets" {
   value = module.hub_vnet.subnet_ids
 }
+
+# ---- DEV Spoke VNet ----
+output "spoke_dev_vnet_id" {
+  value = module.spoke_dev.vnet_id
+}
+
+output "spoke_dev_subnets" {
+  value = module.spoke_dev.subnet_ids
+}
+
+# ---- PROD Spoke VNet ----
+output "spoke_prod_vnet_id" {
+  value = module.spoke_prod.vnet_id
+}
+
+output "spoke_prod_subnets" {
+  value = module.spoke_prod.subnet_ids
+}
